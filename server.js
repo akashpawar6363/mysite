9967234de17360f3b18ed5b457f227177a738897
Akash Pawar <131381080+akashpawar6363@users.noreply.github.com>
2023-05-06 00:31:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 var path=require('path');
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Application started and Listening on port 3000");
-});
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(___dirname + '/index.html'));
+});
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
 });
